2960a70f135d1f5547b8cfd08c3855b309bf220f
Hamza LAMTOUNI <hamza.lamtouni@hotmail.fr>
2021-04-07 19:36:56 +0100

    commit

--- Disease_Detection_WebAPP.py
+++ Disease_Detection_WebAPP.py
@@ -7,9 +7,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import BernoulliNB
 
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
