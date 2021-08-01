3af9c3df28b5fc6ea07f1b084a38f0931e6d12fd
Hamza LAMTOUNI <80209298+HamzaLamtouni@users.noreply.github.com>
2021-08-01 10:52:11 +0100

    Update

--- Disease_Detection_WebAPP.py
+++ Disease_Detection_WebAPP.py
@@ -24,6 +24,8 @@ maxUploadSize = 200
 st.write("""
 # Disease Detector
 Detect if someone has a Disease using machine learning and python!\n
+Pretty simple project , User can choose type of data he want to display , the type of Machine Learning model he wants to use\n
+and the use variables at the right to see if the output will change (0 : probably not sick , 1 : probably sick) , the use can display the correlation matrix to see which variable will make a change in the output !\n
 By Hamza LAMTOUNI
 """)
 # Open and display an image
